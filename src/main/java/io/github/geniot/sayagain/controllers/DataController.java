544e4d230c9b0a9d665034dfb58091d6176d7c58
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2022-08-26 19:00:18 +0200

    Check branch works

--- src/main/java/io/github/geniot/sayagain/controllers/DataController.java
+++ src/main/java/io/github/geniot/sayagain/controllers/DataController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.SerializationUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,11 @@ public class DataController {
             logger.error(ex.getMessage(), ex);
             return ExceptionUtils.getStackTrace(ex);
         }
+    }
+
+    @GetMapping("/welcome")
+    public String handleWelcome() {
+        return "Branch works";
     }
 }
 
