50ab7d752929eddd494203a40d33bf9923a828d4
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2022-08-31 17:06:12 +0200

    Fix profile name prod for exclusion

--- src/main/java/io/github/geniot/sayagain/controllers/TestingController.java
+++ src/main/java/io/github/geniot/sayagain/controllers/TestingController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@Profile("!dev")
+@Profile("!prod")
 @RequestMapping("${apiPrefix}/testing")
 public class TestingController extends BaseController {
 
